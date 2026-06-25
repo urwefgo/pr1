95318d18e0bcf53987d34d0718023454befe1fe2
Vasiliev Georgii <gvasilev523@gmail.com>
2026-06-25 14:11:06 +0900

    Динамическая генерация карточек + фильтры и поиск

--- main.js
+++ main.js
@@ -1,4 +1,63 @@
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  document.body.classList.add("dark-theme");
+}
+
 window.addEventListener("DOMContentLoaded", () => {
+
+  const projects = [
+  { id: 1, title: "Сайт-визитка", category: "frontend", description: "Разработка адаптивного одностраничного сайта" },
+  { id: 2, title: "Таск-трекер", category: "backend", description: "Простое приложение для управления задачами" },
+  { id: 3, title: "Макет мобильного приложения", category: "design", description: "Проектирование пользовательского интерфейса"},
+  { id: 4, title: "Игра на чистом JS", category: "frontend", description: "Разработка интерактивной мини-игры"}
+];
+
+const container = document.getElementById("project-container");
+
+function renderProject(data) {
+  if (!container) return;
+
+  container.innerHTML = "";
+
+  data.forEach((project, index) => {
+    const html = `
+    <article style="animation-delay: ${index * 0.15}s;">
+      <h3>${project.title}</h3>
+      <p>${project.description}</p>
+      </article>
+      `;
+      container.insertAdjacentHTML("beforeend", html);
+  });
+}
+
+renderProject(projects);
+
+const filterButtons = document.querySelectorAll(".filters button");
+
+filterButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    filterButtons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const filter = btn.dataset.filter;
+    const filtered = filter === "all"
+      ? projects
+      : projects.filter(p => p.category === filter);
+
+    renderProject(filtered);
+  });
+});
+
+const searchInput = document.getElementById("search-input");
+
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value.trim().toLowerCase();
+  const filtered = projects.filter(p =>
+    p.title.toLowerCase().includes(query)
+  );
+  renderProject(filtered);
+});
+
 const themeToggle = document.getElementById("theme-toggle");
 
 themeToggle.addEventListener("click", () => {
@@ -6,11 +65,6 @@ themeToggle.addEventListener("click", () => {
   const isDark = document.body.classList.contains("dark-theme");
   localStorage.setItem("theme", isDark ? "dark" : "light");
 });
-
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark") {
-  document.body.classList.add("dark-theme");
-}
 
 console.log("Привет! Скрипт подключен и работает.");
 
@@ -49,6 +103,7 @@ toggleBtn.addEventListener("click", () => {
 });
 
 const form = document.getElementById("contact-form");
+
 if (form) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -82,3 +137,4 @@ form.addEventListener("submit", (event) => {
 });
 }
 });
+
